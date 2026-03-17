7651c27c4bfd9cf064704e8e09ecfb73ba6aa0bd
Alex <flupper825@gmail.com>
2026-03-17 15:17:45 +0300

    Updates

---
+++ assets/Life-of-camp-Bl3BPTIM.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CPe-E0qv.js";var t=e();function n(){return console.log(`Ты в Life-of-camp`),(0,t.jsx)(`div`,{children:(0,t.jsx)(`h1`,{children:`Жизнь лагеря`})})}export{n as default};
